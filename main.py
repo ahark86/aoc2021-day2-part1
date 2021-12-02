7e333731fed6a3988060f93bfb3305e8d85ea10c
Alex Harker <alexnharker@gmail.com>
2021-12-02 10:54:30 -0500

    initialize repo

---
+++ main.py
@@ -0,0 +1,18 @@
+with open('input.txt') as input_file:
+    input = [step.strip().split() for step in input_file.readlines()]
+    print(input)
+
+horiz_pos = 0
+vert_pos = 0
+
+for step in input:
+    if step[0] == 'forward':
+        horiz_pos += int(step[1])
+    if step[0] == 'down':
+        vert_pos += int(step[1])
+    if step[0] == 'up':
+        vert_pos -= int(step[1])
+
+print(horiz_pos)
+print(vert_pos)
+print(horiz_pos * vert_pos)
